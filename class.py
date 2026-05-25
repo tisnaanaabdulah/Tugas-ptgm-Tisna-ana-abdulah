92b9e1d7b789e6b1d1adfa41af4071fdbed858d7
tisnaanaabdulah <tisnaanaabdulah10@gmail.com>
2026-05-25 13:11:55 +0700

    Add files via upload

---
+++ class.py
@@ -0,0 +1,33 @@
+#python 3.7.1
+
+class sekolah :
+  pass
+sekolah1 = sekolah()
+sekolah1.nama = "SMK PGRI 35 SOLOKAN JERUK"
+sekolah1.alamat = "JL RHO Kosasih No 90 Cibodas"
+sekolah2 = sekolah()
+sekolah2.nama = "SMKN 9 Bandung"
+sekolah2.alamat = "Jl Soekarno-Hatta No 10]"
+print(sekolah1.nama)
+print(sekolah1.alamat)
+print(sekolah2.nama)
+print(sekolah2.alamat)
+
+print(type(sekolah1))
+print(type(sekolah2))
+
+class siswa :
+  pass
+siswa1 = siswa()
+siswa1.nama = "Kelpin"
+siswa1.alamat = "Kp cisaradan girang"
+siswa2 = siswa()
+siswa2.nama = "Ramdani"
+siswa2.alamat = "Kp cisaradan girang"
+print(siswa1.nama)
+print(siswa1.alamat)
+print(siswa2.nama)
+print(siswa2.alamat)
+
+print(type(siswa1))
+print(type(siswa2))
---
+++ objek.py
@@ -0,0 +1,27 @@
+class AlatMusik:
+     nama1 = "suling"
+     bahan1 = "bambu"
+     asal1 = "Jawabarat"
+     fungsi1 = "untuk hiburan"
+     penggunaan1 = "ditiup"
+    
+     nama2 = "suling"
+     bahan2 = "bambu"
+     asal2 = "Jawabarat"
+     fungsi2 = "untuk hiburan"
+     penggunaan2 = "ditiup"
+   
+Alat1 = AlatMusik()
+Alat2 = AlatMusik()
+ 
+print(Alat1.nama1)
+print(Alat1.bahan1)
+print(Alat1.asal1)
+print(Alat1.fungsi1)
+print(Alat1.penggunaan1)
+ 
+print(Alat2.nama2)
+print(Alat2.bahan2)
+print(Alat2.asal2)
+print(Alat2.fungsi2)
+print(Alat2.penggunaan2)
