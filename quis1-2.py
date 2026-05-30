83a63ed4cfacd87f8654f61844ad88b309b9f24a
tisnaanaabdulah <tisnaanaabdulah10@gmail.com>
2026-05-30 12:21:42 +0700

    Add files via upload

---
+++ quis1-2.py
@@ -0,0 +1,13 @@
+print ("No. absen: 28")
+print ("Nama: tiara")
+print ("kelas: xi rpl 1")
+print ("sekolah: SMK PGRI 35")
+print ("mata pelajaran: ptgm") 
+print ("alamat: kp pancaksuji")
+
+a=10
+b=5
+c=a+b
+print (a)
+print (b)
+print ("hasil penjumlahan a+b adalah:",c )
